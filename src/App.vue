30c23515406db7f90a5cee88094918beef395861
Cesar Marinho <cesar_marinho@yahoo.com>
2021-04-19 11:37:29 -0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app"><ExampleSix /></div>
+</template>
+
+<script>
+import ExampleSix from "./components/example-six";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+export default {
+  name: "App",
+  components: {
+    ExampleSix
+  }
+};
+</script>
